f12602a7f28b9321fae468e443bd1beb73602fa3
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-16 02:05:32 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -10,4 +10,4 @@ end
 # map goes through each k and v, making an array of all the keys. If the argument passed down includes the value
 #(arguments.include? (value)), the key for that value is shown within the array. Else (:) "nil" is shown
 #in its corresponding place in the array. #compact cuts out all the "nil"s, leaving only the relevant
-#value (the one that corresponds to the argument passed down)
+#key (the one that corresponds to the argument passed down)
